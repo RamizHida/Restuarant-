aa8da0ff285f50bc21c597baf268f0dd04f76b4c
Ramiz Hida <ramizhida00@gmail.com>
2022-11-07 13:54:56 +0100

    Removed elements from HTML and created them using JS

---
+++ src/homepage.js
@@ -0,0 +1,72 @@
+let content = document.querySelector("#content");
+
+// Adding and styline Nav Bar //
+let nav = document.createElement("nav");
+nav.classList.add("nav");
+let navH1 = document.createElement("h1");
+navH1.textContent = "Betim's Bureks!";
+navH1.classList.add("title");
+
+let navH2menu = document.createElement("h2");
+navH2menu.textContent = "Menu";
+
+let navH2Contact = document.createElement("h2");
+navH2Contact.textContent = "Contact";
+
+nav.append(navH1, navH2menu, navH2Contact);
+
+// Adding and styling Intro //
+let mainIntroDiv = document.createElement("div");
+mainIntroDiv.classList.add("intro");
+// Intro Div 1
+let introDiv1 = document.createElement("div");
+let introDiv1H2 = document.createElement("h2");
+introDiv1H2.classList.add("name");
+
+let introDiv1span = document.createElement("span");
+introDiv1span.classList.add("secret");
+introDiv1span.textContent = "Betim";
+introDiv1H2.textContent = ` ${introDiv1span.textContent}: Restaurant Owner`;
+
+let introDiv1Img = document.createElement("img");
+introDiv1Img.id = "chef";
+introDiv1Img.src = "../src/images/chef-betim.png";
+introDiv1Img.alt = "A picture of a chef";
+
+introDiv1.append(introDiv1H2, introDiv1Img);
+
+// Intro Div 2
+let intoDiv2 = document.createElement("div");
+intoDiv2.classList.add("rest-desc");
+
+let introDiv2span = document.createElement("span");
+introDiv2span.classList.add("secret");
+introDiv2span.textContent = "secret";
+let introDiv2para = document.createElement("p");
+introDiv2para.textContent = `Whether you are new to Balkan dishes, or consider yourself a master chef when it comes to cooking them, at Betim's Bureks we guarnetee to satisify your appetite and, more importantly, your taste buds! We believe nothing is more enjoyable than a delicious meal overseeing a beautiful sight. We pride ourselves as having the best bureks in all the Balkans, and some critics say all of Europe! What makes a delicious burek? Fresh ingreidents, patience, and the esstenial ingredient, my ${introDiv2span.textContent} recipe! Don't take my word for it, come and try one yourself! We are a family-owned business and have been making stomachs happy and full since 1889. From my family to yours, we hope to see you soon!`;
+
+intoDiv2.append(introDiv2para);
+
+// Adding introDiv1 and introDiv2 to main intro
+mainIntroDiv.append(introDiv1, intoDiv2);
+
+// Adding and styling last div section, which is beach //
+let beachDiv = document.createElement("div");
+let beachDivH3 = document.createElement("h3");
+beachDivH3.classList.add("title");
+beachDivH3.textContent = "Where You Will Be Eating";
+
+let beachImg = document.createElement("img");
+beachImg.id = "beach-img";
+beachImg.src = "../src/images/balkan-beach.png";
+beachImg.alt =
+  "A picture of an old Balkan style building near a body of water and a valley next to it";
+
+beachDiv.append(beachDivH3, beachImg);
+
+// Appending all sections//
+content.append(nav, mainIntroDiv, beachDiv);
+
+export function loadPage() {
+  return content;
+}
